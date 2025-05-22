8a7b43b920c8e7f945acbd3fb2291d81f06b25f2
DivyGupta27 <divygupta2002@gmail.com>
2025-05-22 18:34:56 +0530

    rooms

--- src/components/ShowRooms.jsx
+++ src/components/ShowRooms.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import RoomCard from './RoomCard';
+import myroom from '../Booking.rooms.json'
 
 const ShowRooms = ({type}) => {
   const [rooms, setRooms] = useState([]);
@@ -22,7 +23,7 @@ const ShowRooms = ({type}) => {
   return (
     <div className='flex flex-wrap justify-center mx-6 gap-9'>
       {!type?
-      rooms.map((list, i) => (
+      myroom.map((list, i) => (
         <RoomCard name={list.name} price={list.price} key={i} ex1={list.amenities[0]} ex2={list.amenities[1]} ex3={list.amenities[2]} image={list.image}/>
       )): rooms.filter(find=>find.type==type).map((list, i) => (
         <RoomCard name={list.name} price={list.price} key={i} ex1={list.amenities[0]} ex2={list.amenities[1]} ex3={list.amenities[2]} image={list.image}/>
