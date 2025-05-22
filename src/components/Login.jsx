f8739b869ecc5c6f60bfd5fc68be26fb8b266741
DivyGupta27 <divygupta2002@gmail.com>
2025-05-22 18:43:38 +0530

    done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,8 +5,8 @@ const Login = () => {
     <div className='flex justify-center '>
         <div className="flex flex-col max-w-md p-6 border mt-3 rounded-md sm:p-10 dark:bg-gray-50 dark:text-gray-800">
 	<div className="mb-8 text-center ">
-		<h1 className="my-3 text-4xl font-bold">Sign in</h1>
-		<p className="text-sm dark:text-gray-600">Sign in to access your account</p>
+		<h1 className="my-3 text-4xl font-bold">Login</h1>
+		<p className="text-sm dark:text-gray-600">Login to access your account</p>
 	</div>
 	<form noValidate="" action="" className="space-y-12">
 		<div className="space-y-4">
@@ -24,7 +24,7 @@ const Login = () => {
 		</div>
 		<div className="space-y-2">
 			<div>
-				<button type="button" className="w-full px-8 py-3 font-semibold text-white rounded-md bg-red-500 dark:bg-violet-600 dark:text-gray-50">Sign in</button>
+				<button type="button" className="w-full px-8 py-3 font-semibold text-white rounded-md bg-red-500 ">Login</button>
 			</div>
 			<p className="px-6 text-sm text-center dark:text-gray-600">Don't have an account yet?{" "}
 				<a rel="noopener noreferrer" href="signup" className="text-red-500 hover:underline">Sign up</a>.
